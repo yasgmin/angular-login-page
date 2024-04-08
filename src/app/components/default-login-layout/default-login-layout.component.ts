5851d97c588ed23e72651a2b8eddf477c48ff601
Yasmin Gonçalves <MIMINDZN@GMAIL.COM>
2024-04-08 08:55:23 -0300

    login-page-angular-v2

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
   standalone: true,
   imports: [],
   templateUrl: './default-login-layout.component.html',
-  styleUrl: './default-login-layout.component.scss'
+  styleUrl: './default-login-layout.component.scss',
 })
 export class DefaultLoginLayoutComponent {
-
+  @Input() title: string = '';
+  @Input() primaryBtnText: string = '';
+  @Input() secundaryBtnText: string = '';
 }
---
+++ src/app/components/primary-input/primary-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './primary-input.component.html',
+  styleUrl: './primary-input.component.scss'
+})
+export class PrimaryInputComponent {
+
+}
